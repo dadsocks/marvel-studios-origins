4f91ffbfde60c43300754164586ed73db361d5d2
Bill Taggart <taggarw@gmail.com>
2018-03-18 15:55:49 -0700

    Added TMDB Query to Grab Character Names On Movie Poster Click

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ function getMarvelMovieData(callback) {
   $.getJSON(companyMoviesUrl,callback);
 }
 
+function getCharacterData(movieId,callback) {
+  const charactersUrl = `${TMDB_SEARCH_URL}movie/${movieId}/credits?api_key=a6f231ffb0d29fd46f9500b4b138e82c&language=en-US`;
+
+  $.getJSON(charactersUrl,callback);
+}
+
 
 function renderMovies(result) {
   return `<li>
-      <img class="movie scroll-item" src="https://image.tmdb.org/t/p/w500/${result.poster_path}" alt="${result.original_title}">
-  </li>
-  `
+            <a href="#" id="${result.id}" class="moviePoster">
+              <img class="movie scroll-item" src="https://image.tmdb.org/t/p/w500/${result.poster_path}" alt="${result.original_title}">
+            </a>
+          </li>`
 }
 
 function marvelMovieData(data) {
@@ -33,8 +40,46 @@ function marvelMovieData(data) {
 }
 
 
+function getCharacters(data) {
+  //pulls character names from TMDB based on movie selection
+  const characterNames = data.cast.map(cast => cast.character);
+  //Cleans up name for use with Marvel API
+  const cleanCharacterNames = characterNames.map(function(name) {
+    if(name.includes('/') && name.includes('(voice)')) {
+      let alias = name.split('/ ');
+      alias = alias[alias.length - 1].split(' ');
+      return alias[0];
+    } else if(name.includes('/')) {
+      let alias = name.split('/ ');
+      return alias[alias.length - 1];
+    } else if(name.includes('(voice)')) {
+      let alias = name.split(' ');
+      return alias[0];
+    } else if(name.includes('Thor')) {
+      let alias = name.split(' ');
+      return alias[0];
+    }
+    return name;
+  });
+  console.log(cleanCharacterNames);
+
+  //run function to query MarvelAPI forEach cleanCharacterName
+
+  //run function to append HTML to DOM
+
+}
+
+function selectMovie() {
+  $('.movie-list').on('click','.moviePoster', event => {
+    const movieID = $(event.currentTarget).attr('id');
+    getCharacterData(movieID,getCharacters);
+  });
+}
+
+
 function renderPage() {
   getMarvelMovieData(marvelMovieData);
+  selectMovie();
 }
 
 $(renderPage);
